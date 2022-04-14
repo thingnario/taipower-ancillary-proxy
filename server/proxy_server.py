f6da5ff056bd74782f1dacd6c035681208fff899
Mike Su <kyatapi@gmail.com>
2022-04-14 18:16:14 +0800

    Extract proto_servicer into a file

--- server/proxy_server.py
+++ server/proxy_server.py
@@ -9,37 +9,7 @@ import taipower_ancillary_pb2_grpc
 from model_loader import (UPDATERS,
                           load_model,
                           load_logical_device)
-
-
-class AncillaryInputsServicer(taipower_ancillary_pb2_grpc.AncillaryInputsServicer):
-    def __init__(self, servant):
-        self._servant = servant
-
-    @staticmethod
-    def _load_logical_devices(devices):
-        for device in devices:
-            yield {
-                'name': device.name,
-                'logical_nodes': json.loads(device.logical_nodes),
-            }
-
-    def update_point_values(self, request, context):
-        self._servant.update_value(json.loads(request.values))
-        return taipower_ancillary_pb2.Response(success=True)
-
-    def add_logical_devices(self, request, context):
-        devices = list(AncillaryInputsServicer._load_logical_devices(request.devices))
-        self._servant.add_logical_devices(devices)
-        return taipower_ancillary_pb2.Response(success=True)
-
-    def reset_logical_devices(self, request, context):
-        devices = list(AncillaryInputsServicer._load_logical_devices(request.devices))
-        self._servant.reset_logical_devices(devices)
-        return taipower_ancillary_pb2.Response(success=True)
-
-    def restart_ied_server(self, request, context):
-        self._servant.restart_ied_server()
-        return taipower_ancillary_pb2.Response(success=True)
+from proto_servicer import AncillaryInputsServicer
 
 
 class ProxyServer():
